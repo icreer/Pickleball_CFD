6bb6d3ed2b36322a8c9f14407d30209f05ba3cca
Isaac Creer <isaaccreer@gmail.com>
2023-06-26 15:02:43 -0600

    Test window application

---
+++ main.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+
+# Creating a window in Tkinter
+window = tk.Tk()
+
+window.title("Test")
+
+
+
+window.geometry("700x500")
+
+'''
+# will be useful for creating the widow state that not in fix diemtions
+width = window.winfo_screenwidth()               
+height = window.winfo_screenheight()               
+window.geometry("%dx%d" % (width, height))
+'''
+
+# Create a label widget in Tkinter
+label = tk.Label(window, text="Click the Button to update this Text",
+font=('Calibri 15 bold'))
+label.pack(pady=20)
+ 
+# Function to update the label text for first button click in Tkinter
+def on_click_btn1():
+    label["text"] = "You clicked first button"
+     
+# Function to update the label text for second button click in Tkinter
+def on_click_btn2():
+    label["text"] = "You clicked second button"
+     
+# Create 1st button to update the label widget
+btn1 = tk.Button(window, text="Button1", command=on_click_btn1)
+btn1.pack(pady=20)
+ 
+# Create 2nd button to update the label widget
+btn2 = tk.Button(window, text="Button2", command=on_click_btn2)
+btn2.pack(pady=40)
+ 
+# Run main loop
+window.mainloop()
