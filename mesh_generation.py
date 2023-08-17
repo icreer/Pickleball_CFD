c91681814ad07779b5ec200ccb20ae87e383352e
Isaac Creer <isaaccreer@gmail.com>
2023-08-16 23:42:40 -0600

    small update

--- mesh_generation.py
+++ mesh_generation.py
@@ -1,5 +1,6 @@
 '''
 This File Job is generate the mesh for each case. Note each mesh is based of the standared size of a pickle ball
+Code is pulled from https://github.com/icreer/mesh_generation
 '''
 
 import pyvista as vista 
